da61c9ebe0a171d977618a8a498c20d982395e3a
Persulence <persulence@proton.me>
2025-06-04 19:04:25 +0100

    gui: Add custom slider widget

---
+++ src/widget/SliderWidget.cpp
@@ -0,0 +1,74 @@
+#include "SliderWidget.hpp"
+
+namespace ui
+{
+    void SliderWidget::setRange(const Value min_, const Value max_, const Value increment_)
+    {
+        min = min_;
+        max = max_;
+        increment = increment_;
+    }
+
+    void SliderWidget::mouseDown(const juce::MouseEvent &event)
+    {
+        dragValue = current;
+
+        if (event.mods.isRightButtonDown())
+        {
+            mouseDownValue(event);
+        }
+    }
+
+    void SliderWidget::mouseDrag(const juce::MouseEvent &event)
+    {
+        const auto dx = event.getDistanceFromDragStartX();
+        if (event.mods.isLeftButtonDown())
+        {
+            const Value sensitivity = 0.05;
+            snapValue(dragValue + sensitivity * dx);
+        }
+        else if (event.mods.isRightButtonDown())
+        {
+            mouseDownValue(event);
+        }
+    }
+
+    void SliderWidget::paint(juce::Graphics &g)
+    {
+        Value f = current / (std::abs(max - min));
+        auto rect = getLocalBounds().toFloat().expanded(-0.5).withWidth(f * getWidth());
+        g.setColour(juce::Colours::red);
+        g.fillRoundedRectangle(rect, 5);
+        g.setColour(juce::Colours::white);
+        g.drawText(juce::String{current}, getX(), getY(), getWidth(), getHeight(), juce::Justification::centred, true);
+    }
+
+    void SliderWidget::mouseDownValue(const juce::MouseEvent &event)
+    {
+        const auto bounds = getLocalBounds();
+        const double f = (event.x - bounds.getX()) / static_cast<double>(bounds.getWidth());
+        snapValue(min + f * (max - min));
+    }
+
+    void SliderWidget::snapValue(Value value)
+    {
+        auto oldCurrent = current;
+        if (increment > 0)
+        {
+            auto div = value / increment;
+            auto rounded = std::round(div);
+            current = std::clamp(rounded * increment, min, max);
+        }
+        else
+        {
+            current = std::clamp(value, min, max);
+        }
+
+        if (current != oldCurrent)
+        {
+            // notify
+            onChanged.emit(current);
+            repaint();
+        }
+    }
+}
---
+++ src/widget/SliderWidget.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <juce_gui_basics/juce_gui_basics.h>
+
+#include "signal/Signal.hpp"
+
+namespace ui
+{
+    class SliderWidget : public juce::Component
+    {
+        using Value = double;
+        using C = signal_event::Callback<Value>;
+
+        Value min{};
+        Value max{1};
+        Value current{};
+        Value dragValue{};
+        Value increment{};
+
+    public:
+        C::Signal onChanged;
+
+        SliderWidget() = default;
+
+        void setRange(Value min_, Value max_, Value increment_);
+
+        void mouseDown(const juce::MouseEvent &event) override;
+        void mouseDrag(const juce::MouseEvent &event) override;
+        void paint(juce::Graphics &g) override;
+
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SliderWidget)
+
+    private:
+        void mouseDownValue(const juce::MouseEvent& event);
+        void snapValue(Value value);
+    };
+}
