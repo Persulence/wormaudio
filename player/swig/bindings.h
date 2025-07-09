5e2cc9601db6d79f6d9ce08b920e457697f75b13
Persulence <persulence@proton.me>
2025-07-09 11:25:56 +0100

    cmake: Output path fiddling

--- player/swig/bindings.h
+++ player/swig/bindings.h
@@ -5,8 +5,25 @@
 // #include "resource/Project.hpp"
 // #include "resource/SharedResource.hpp"
 
-inline void ooer()
+inline void ooere()
 {
     // resource::Handle<resource::Project> i = resource::make<resource::Project>();
     std::cout << "ooer\n";
 }
+
+class Classe
+{
+public:
+    int i;
+
+    void memberFunction()
+    {
+        std::cout << "memberFunction\n";
+    }
+
+    const Classe* makeCopie() const
+    {
+        // return new Classe{i};
+        return this;
+    }
+};
--- player/swig/bindings.i
+++ player/swig/bindings.i
@@ -1,9 +1,21 @@
-%module swig_example
+%module neepsound
 
 // Add necessary symbols to generated header
 %{
 #include "bindings.h"
 %}
+
+%pragma(java) jniclasscode=%{
+  static {
+    try {
+        System.loadLibrary("neepsound");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println("Native code library failed to load. \n" + e);
+      System.exit(1);
+    }
+  }
+%}
+
 
 // Process symbols in header
 %include "bindings.h"
