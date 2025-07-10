dd0113cf35951cb1d36381283776827fa5f6b5b6
Persulence <persulence@proton.me>
2025-07-10 10:11:57 +0100

    cmake: add USE_SWIG_DEPENDENCIES to bindings.i

--- player/compile_java.sh
+++ player/compile_java.sh
@@ -5,14 +5,14 @@ output_dir=$(realpath -s "$2")
 jar_name=$3
 build_dir="$input_dir/build"
 
-cd "$input_dir" || exit
+cd "$input_dir" || exit 1
 
 mkdir -p build
-
+find ./build -name "*.class" -exec rm {} \;
 javac -d ./build ./*.java
 
-cd "$output_dir" || exit
+cd "$output_dir" || exit 1
 rm -f "$jar_name"
-jar cvf "$jar_name" -C "$build_dir" .
+jar cvf "$jar_name" -C "$build_dir" . || exit 1
 
 #cp "$jar_name" ../../../java_test/
--- player/swig/bindings.h
+++ player/swig/bindings.h
@@ -5,13 +5,24 @@
 // #include "resource/Project.hpp"
 #include "event/EventDef.hpp"
 #include "resource/SharedResource.hpp"
-#include "java/java_alloc.hpp"
+// #include "java/java_alloc.hpp"
 
 inline void ooere()
 {
     // resource::Handle<resource::Project> i = resource::make<resource::Project>();
     std::cout << "ooer\n";
 }
+
+class SoundThing
+{
+public:
+    int field;
+
+    void memberFunction() const
+    {
+        std::cout << "field: " << field << "\n";
+    }
+};
 
 class NEventDef;
 
@@ -22,6 +33,11 @@ class NSoundInstance
 
 public:
     explicit NSoundInstance(const NEventDef* parent);
+
+    ~NSoundInstance()
+    {
+        std::cout << "NSoundInstance destroyed\n";
+    }
 };
 
 class NEventDef
@@ -29,18 +45,29 @@ class NEventDef
     resource::Handle<event::EventDef> eventDef;
 
 public:
-    NSoundInstance* instantiate() const;
+    NEventDef(): eventDef(event::EventDef::create())
+    {
+        std::cout << "NEventDef created\n";
+    }
+
+    NSoundInstance instantiate() const;
 
     friend class NSoundInstance;
+
+    ~NEventDef()
+    {
+        std::cout << "NEventDef destroyed\n";
+    }
 };
 
 inline NSoundInstance::NSoundInstance(const NEventDef *parent):
     parent(parent)
 {
+    std::cout << "NSoundInstance created\n";
     instance = parent->eventDef->instantiate();
 }
 
-inline NSoundInstance * NEventDef::instantiate() const
+inline NSoundInstance NEventDef::instantiate() const
 {
-    return new NSoundInstance{this};
+    return NSoundInstance{this};
 }
