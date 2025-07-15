e174c19f136ac7c6fc8a11b37f49dec0db86b2e5
Persulence <persulence@proton.me>
2025-07-15 12:36:10 +0100

    player: Made MessageThreadManager more complete

--- player/src/api/MessageThreadManager.hpp
+++ player/src/api/MessageThreadManager.hpp
@@ -7,16 +7,14 @@
 #include <juce_core/juce_core.h>
 #include <juce_events/juce_events.h>
 
-#include "signal/Signal.hpp"
+#include "runtime/Runtime.hpp"
 
 namespace wa
 {
     class MessageThreadManager
     {
     public:
-        using OnClose = signal_event::Callback<void>;
-
-        OnClose::Signal onClose;
+        // using OnClose = signal_event::Callback<>;
 
         MessageThreadManager()
         {
@@ -26,25 +24,72 @@ namespace wa
         {
         }
 
-        static void claimMessageThread()
+        void initContext()
         {
-            juce::Logger::writeToLog("WormAudio: Initialising message thread");
-            juce::MessageManager::getInstance()->setCurrentThreadAsMessageThread();
-            juce::MessageManager::getInstance()->runDispatchLoop();
-        }
+            scope = std::make_unique<juce::ScopedJuceInitialiser_GUI>();
 
-        void requestMessageThreadStop()
-        {
-            juce::MessageManager::callAsync([this]
+            messageThread = std::make_unique<std::thread>([&]
             {
-                onClose.emit();
-                juce::MessageManager::getInstance()->stopDispatchLoop();
+                juce::Logger::writeToLog("WormAudio: Initialising message thread");
+                juce::MessageManager::getInstance()->setCurrentThreadAsMessageThread();
+                juce::MessageManager::getInstance()->runDispatchLoop();
+            });
+
+            juce::MessageManager::callAsync([&]
+            {
+                juce::Logger::writeToLog("WormAudio: Initialising runtime from message thread");
+                runtime = std::make_unique<runtime::Runtime>();
+                runtime->connectToDevice();
             });
         }
+
+        void deInitContext()
+        {
+            if (messageThread)
+            {
+                juce::MessageManager::callAsync([&]
+                {
+                    juce::Logger::writeToLog("WormAudio: Stopping message thread");
+                    runtime = nullptr;
+                    juce::MessageManager::getInstance()->stopDispatchLoop();
+
+                });
+
+                if (messageThread->joinable())
+                    messageThread->join();
+
+                scope = nullptr;
+            }
+        }
+
+        runtime::Runtime & getRuntime() const
+        {
+            assert(runtime);
+
+            return *runtime;
+        }
+
+        // static void claimMessageThread()
+        // {
+        //     juce::Logger::writeToLog("WormAudio: Initialising message thread");
+        //     juce::MessageManager::getInstance()->setCurrentThreadAsMessageThread();
+        //     juce::MessageManager::getInstance()->runDispatchLoop();
+        // }
+        //
+        // void requestMessageThreadStop()
+        // {
+        //     juce::MessageManager::callAsync([this]
+        //     {
+        //         onClose.emit();
+        //         juce::MessageManager::getInstance()->stopDispatchLoop();
+        //     });
+        // }
 
     private:
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MessageThreadManager)
 
-        juce::ScopedJuceInitialiser_GUI scope{};
+        std::unique_ptr<juce::ScopedJuceInitialiser_GUI> scope{};
+        std::unique_ptr<std::thread> messageThread;
+        std::unique_ptr<runtime::Runtime> runtime;
     };
 }
