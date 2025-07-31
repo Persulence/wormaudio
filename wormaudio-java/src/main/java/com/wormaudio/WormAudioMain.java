58f0b6e297d47f63d9dca4fef5df8291b8b37540
Persulence <persulence@proton.me>
2025-07-31 16:02:02 +0100

    Update license

--- wormaudio-java/src/main/java/com/wormaudio/WormAudioMain.java
+++ wormaudio-java/src/main/java/com/wormaudio/WormAudioMain.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c)  2025 Thomas Paley-Menzies
+ *
+ * This file is part of the WormAudio project.
+ *
+ * WormAudio is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * WormAudio is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Foobar. If not, see https://www.gnu.org/licenses/.
+ *
+ */
+
 package com.wormaudio;
 
 import com.wormaudio.wormaudio.WormAudioContext;
