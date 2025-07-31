e0b2bd8eece85fff01c618742d256d5d55b8e8ed
Persulence <persulence@proton.me>
2025-07-31 16:02:02 +0100

    Update license

--- wormaudio/src/state/Parameter.cpp
+++ wormaudio/src/state/Parameter.cpp
@@ -1,6 +1,11 @@
-// Copyright (c) 2025 Persulence
+// Copyright (c)  2025 Persulence
 //
-// This work is licensed under CC BY-NC-SA 4.0. To view a copy of the license, visit https://creativecommons.org/licenses/by-nc-sa/4.0
+// This file is part of the WormAudio project.
+//
+// WormAudio is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. WormAudio is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with Foobar. If not, see https://www.gnu.org/licenses/.
+//
 
 #include "Parameter.hpp"
 
